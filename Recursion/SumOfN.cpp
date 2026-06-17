24c34a6ae305084752ef714dcb596ef2ce0d9f13
Anukriti Pandey <2501030084@mail.jiit.ac.in>
2026-06-17 21:48:44 +0530

    Recursion completed

--- Recursion/SumOfN.cpp
+++ Recursion/SumOfN.cpp
@@ -11,10 +11,22 @@ int Sum(int n, int sum)
     Sum(n,sum);
     
 }
+//functional approach
+int sumnum(int n)
+{
+    if(n==0)
+    {
+        return n;
+    }
+    n=n+sumnum(n-1);
+    return n;
+}
 int main()
 {
     int n=5;
     int sum=0;
     int a=Sum(n,sum);
     cout<<"the sum is "<<a<<endl;
+    int x=sumnum(n);
+    cout<<x<<endl;
 }
