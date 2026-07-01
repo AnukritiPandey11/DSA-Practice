5a52d35015a4e90f155a471aa0e1c2de8be11354
Anukriti Pandey <2501030084@mail.jiit.ac.in>
2026-07-01 22:17:12 +0530

    Array Question(Medium)

---
+++ Array[Medium]/MaximumSubarray.cpp
@@ -0,0 +1,24 @@
+//LeetCode-53
+//Kadane's Algo
+//TC-O(N)
+//SC-O(1)
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long maxi=LONG_MIN;
+        long long sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum=sum+nums[i];
+            maxi=max(sum,maxi);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxi;
+        
+    }
+};
